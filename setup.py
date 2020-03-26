ecb79dc58bef257ced4b25891037c8b727fbe6bd
Kentaro Wada <www.kentaro.wada@gmail.com>
2020-03-26 17:18:28 +0000

    Initialize the project

---
+++ setup.py
@@ -0,0 +1,83 @@
+from __future__ import print_function
+
+import distutils.spawn
+from setuptools import find_packages
+from setuptools import setup
+import shlex
+import subprocess
+import sys
+
+
+github_slug = "wkentaro/video-cli"
+name = "video-cli"
+version = "0.1"
+
+
+if sys.argv[1] == "release":
+    if not distutils.spawn.find_executable("twine"):
+        print(
+            "Please install twine:\n\n\tpip install twine\n", file=sys.stderr
+        )
+        sys.exit(1)
+
+    commands = [
+        "git pull origin master",
+        "git tag v{:s}".format(version),
+        "git push origin master --tag",
+        "python setup.py sdist",
+        "twine upload dist/{:s}-{:s}.tar.gz".format(name, version),
+    ]
+    for cmd in commands:
+        subprocess.check_call(shlex.split(cmd))
+    sys.exit(0)
+
+
+def get_long_description():
+    with open("README.md") as f:
+        long_description = f.read()
+
+    try:
+        import github2pypi
+
+        return github2pypi.replace_url(
+            slug=github_slug, content=long_description
+        )
+    except Exception:
+        return long_description
+
+
+def get_install_requires():
+    install_requires = []
+    with open("requirements.txt") as f:
+        for req in f:
+            install_requires.append(req.strip())
+    return install_requires
+
+
+setup(
+    name=name,
+    version=version,
+    packages=find_packages(exclude=["github2pypi"]),
+    install_requires=get_install_requires(),
+    description="Command line tools for quick video editing.",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    author="Kentaro Wada",
+    author_email="www.kentaro.wada@gmail.com",
+    url="http://github.com/{:s}".format(github_slug),
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
+    entry_points={
+        "console_scripts": ["video_retime=video_cli.video_retime:main"]
+    },
+)
---
+++ video_cli/video_retime.py
@@ -0,0 +1,52 @@
+import argparse
+import os.path as osp
+
+import imageio
+import tqdm
+
+
+def get_macro_block_size(size):
+    for macro_block_size in range(16, 1, -1):
+        if size[0] % macro_block_size == 0 and size[1] % macro_block_size == 0:
+            break
+    return macro_block_size
+
+
+def video_retime(in_file, retime, inplace=False):
+    stem, ext = osp.splitext(in_file)
+    out_file = stem + "_retime{:.3g}".format(retime) + ext
+
+    reader = imageio.get_reader(in_file)
+    meta_data = reader.get_meta_data()
+    fps = meta_data["fps"]
+
+    macro_block_size = get_macro_block_size(meta_data["size"])
+
+    writer = imageio.get_writer(
+        out_file, fps=fps * retime, macro_block_size=macro_block_size
+    )
+    for data in tqdm.tqdm(reader, total=reader.count_frames()):
+        writer.append_data(data)
+
+    reader.close()
+    writer.close()
+
+    if inplace:
+        raise NotImplementedError
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument("in_files", nargs="+", help="input files")
+    parser.add_argument(
+        "--retime", "-x", type=float, default=1, help="retime factor"
+    )
+    parser.add_argument(
+        "--inplace", "-i", action="store_true", help="operate in-place"
+    )
+    args = parser.parse_args()
+
+    for in_file in args.in_files:
+        video_retime(in_file=in_file, retime=args.retime, inplace=args.inplace)
