870da0ecb3f4165e0c741a3a08cd5a7f64686449
Kentaro Wada <www.kentaro.wada@gmail.com>
2020-03-29 06:40:23 +0100

    Fix for linter

--- video_cli/fromimg.py
+++ video_cli/fromimg.py
@@ -8,24 +8,28 @@ import tqdm
 
 
 def natural_sort(l):
-    convert = lambda text: int(text) if text.isdigit() else text.lower()  # NOQA
-    alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]  # NOQA
+    convert = lambda text: int(text) if text.isdigit() else text.lower()
+    alphanum_key = lambda key: [convert(c) for c in re.split("([0-9]+)", key)]
     return sorted(l, key=alphanum_key)
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('out_file')
-    parser.add_argument('-i', '--input-files', default='*.jpg',
-                        help="Input patterns like '*.jpg'")
-    parser.add_argument('--fps', type=int, default=10)
+    parser.add_argument("out_file")
+    parser.add_argument(
+        "-i",
+        "--input-files",
+        default="*.jpg",
+        help="Input patterns like '*.jpg'",
+    )
+    parser.add_argument("--fps", type=int, default=10)
     args = parser.parse_args()
 
     args.input_files = glob.glob(args.input_files)
 
     # fix low fps for non-GIF videos
     n_times_write = 1
-    if osp.splitext(args.out_file)[1] != '.gif' and args.fps < 10:
+    if osp.splitext(args.out_file)[1] != ".gif" and args.fps < 10:
         n_times_write = 10 // args.fps
         args.fps = 10
 
