0ad3ce87fd366507bfd9b333dc39a7f1602ce1ad
Kentaro Wada <www.kentaro.wada@gmail.com>
2020-06-27 16:44:56 +0100

    Fix for flake8

--- video_cli/utils/natural_sort.py
+++ video_cli/utils/natural_sort.py
@@ -1,7 +1,7 @@
 import re
 
 
-def natural_sort(l):
+def natural_sort(lst):
     convert = lambda text: int(text) if text.isdigit() else text.lower()
     alphanum_key = lambda key: [convert(c) for c in re.split("([0-9]+)", key)]
-    return sorted(l, key=alphanum_key)
+    return sorted(lst, key=alphanum_key)
